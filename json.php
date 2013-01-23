1378315b54d33e6ca00825d19562cf0429d691b8
Ryan Yacyshyn <ryan_yacyshyn@yahoo.ca>
2013-01-22 21:31:51 -0800

    added the rest of the symbols, added google analytics, not preloading the audio, added cc..

--- json.php
+++ json.php
@@ -52,9 +52,71 @@ $arr = array(
 			'more' => 'http://en.wikipedia.org/wiki/Voiced_velar_stop'),
 
 	/********************************************************
-	 * frictives..
+	 * fricatives..
 	 */
 
+	array(	'symbol' => 'f',
+			'place' => 'Labiodental',
+			'manner' => 'Fricatives',
+			'voice' => '-V',
+			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/3/33/Voiceless_labiodental_fricative.ogg',
+			'more' => 'http://en.wikipedia.org/wiki/Voiceless_labiodental_fricative'),
+
+	array(	'symbol' => 'v',
+			'place' => 'Labiodental',
+			'manner' => 'Fricatives',
+			'voice' => '+V',
+			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/8/85/Voiced_labiodental_fricative.ogg',
+			'more' => 'http://en.wikipedia.org/wiki/Voiced_labiodental_fricative'),
+
+	array(	'symbol' => 'θ',
+			'place' => 'Dental',
+			'manner' => 'Fricatives',
+			'voice' => '-V',
+			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/8/80/Voiceless_dental_fricative.ogg',
+			'more' => 'http://en.wikipedia.org/wiki/Voiceless_dental_fricative'),
+
+	array(	'symbol' => 'ð',
+			'place' => 'Dental',
+			'manner' => 'Fricatives',
+			'voice' => '+V',
+			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/6/6a/Voiced_dental_fricative.ogg',
+			'more' => 'http://en.wikipedia.org/wiki/Voiced_dental_fricative'),
+
+	array(	'symbol' => 's',
+			'place' => 'Alveolar',
+			'manner' => 'Fricatives',
+			'voice' => '-V',
+			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/a/ac/Voiceless_alveolar_sibilant.ogg',
+			'more' => 'http://en.wikipedia.org/wiki/Voiceless_alveolar_sibilant'),
+
+	array(	'symbol' => 'z',
+			'place' => 'Alveolar',
+			'manner' => 'Fricatives',
+			'voice' => '+V',
+			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/c/c0/Voiced_alveolar_sibilant.ogg',
+			'more' => 'http://en.wikipedia.org/wiki/Voiced_alveolar_sibilant'),
+
+	array(	'symbol' => 'ʃ',
+			'place' => 'Palatal',
+			'manner' => 'Fricatives',
+			'voice' => '-V',
+			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/c/cc/Voiceless_palato-alveolar_sibilant.ogg',
+			'more' => 'http://en.wikipedia.org/wiki/Voiceless_palato-alveolar_sibilant'),
+
+	array(	'symbol' => 'ʒ',
+			'place' => 'Palatal',
+			'manner' => 'Fricatives',
+			'voice' => '+V',
+			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/3/30/Voiced_palato-alveolar_sibilant.ogg',
+			'more' => 'http://en.wikipedia.org/wiki/Voiced_palato-alveolar_sibilant'),
+
+	array(	'symbol' => 'h',
+			'place' => 'Glottal',
+			'manner' => 'Fricatives',
+			'voice' => '-V',
+			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/d/da/Voiceless_glottal_fricative.ogg',
+			'more' => 'http://en.wikipedia.org/wiki/Voiceless_glottal_fricative'),
 
 
 	/********************************************************
@@ -73,6 +135,63 @@ $arr = array(
 			'voice' => '+V',
 			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/e/e6/Voiced_palato-alveolar_affricate.ogg',
 			'more' => 'http://en.wikipedia.org/wiki/Voiced_palato-alveolar_affricate'),
+
+	/********************************************************
+	 * nasals..
+	 */
+
+	array(	'symbol' => 'm',
+			'place' => 'Bilabial',
+			'manner' => 'Nasals',
+			'voice' => '+V',
+			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/a/a9/Bilabial_nasal.ogg',
+			'more' => 'http://en.wikipedia.org/wiki/Bilabial_nasal'),
+
+	array(	'symbol' => 'n',
+			'place' => 'Alveolar',
+			'manner' => 'Nasals',
+			'voice' => '+V',
+			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/2/29/Alveolar_nasal.ogg',
+			'more' => 'http://en.wikipedia.org/wiki/Alveolar_nasal'),
+
+	array(	'symbol' => 'ŋ',
+			'place' => 'Velar',
+			'manner' => 'Nasals',
+			'voice' => '+V',
+			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/3/39/Velar_nasal.ogg',
+			'more' => 'http://en.wikipedia.org/wiki/Velar_nasal'),
+
+	/********************************************************
+	 * approximants..
+	 */
+
+	array(	'symbol' => 'l',
+			'place' => 'Alveolar',
+			'manner' => 'Approximants (Liquids)',
+			'voice' => '+V',
+			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/b/bc/Alveolar_lateral_approximant.ogg',
+			'more' => 'http://en.wikipedia.org/wiki/Alveolar_lateral_approximant'),
+
+	array(	'symbol' => 'ɹ',
+			'place' => 'Alveolar',
+			'manner' => 'Approximants (Liquids)',
+			'voice' => '+V',
+			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/1/1f/Alveolar_approximant.ogg',
+			'more' => 'http://en.wikipedia.org/wiki/Alveolar_approximant'),
+
+	array(	'symbol' => 'w',
+			'place' => 'Bilabial (or Labialized Velar?)',
+			'manner' => 'Approximants (Glides)',
+			'voice' => '+V',
+			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/f/f2/Voiced_labio-velar_approximant.ogg',
+			'more' => 'http://en.wikipedia.org/wiki/Labio-velar_approximant'),
+
+	array(	'symbol' => 'j',
+			'place' => 'Palatal',
+			'manner' => 'Approximants (Glides)',
+			'voice' => '+V',
+			'audio' => 'http://upload.wikimedia.org/wikipedia/commons/e/e8/Palatal_approximant.ogg',
+			'more' => 'http://en.wikipedia.org/wiki/Palatal_approximant'),
 
 	);
 
